0aa7d4528d5d4aeec41a7927fd6ed1666b8b168d
sergey-komar <kursev81@yandex.ru>
2022-06-26 10:09:00 +0300

    work

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,41 @@
 $(function () {
+    $('.question-accardion__btn').on('click', function(){
+        $(this).next().slideToggle(500)
+    });
 
-})
+    $('.slider__box').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1090,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 650,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                
+              }
+            },
+        ]
+    });
+
+       // MOBILE BTN
+        const navIcon = document.querySelector('.nav-icon');
+        const nav = document.querySelector('.menu__list');
+
+        navIcon.addEventListener("click", function () {
+        // document.querySelector('.nav-icon').classList.toggle('active');
+
+        this.classList.toggle("nav-icon--active");
+
+        nav.classList.toggle('nav--active');
+        });
+});
